5ac6aae1c3fefbdc7b9bd2b7a92cc6d725524122
Crystal Sujeong Lee <plane11@users.noreply.github.com>
2017-03-07 18:20:34 +0900

    Create Main.java

---
+++ src/main/java/baekjoon/S1260/Main.java
@@ -0,0 +1,22 @@
+package baekjoon.S1260;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String[] strs = br.readLine().split(" ");
+
+        int N = Integer.parseInt(strs[0]);
+        int M = Integer.parseInt(strs[1]);
+        int V = Integer.parseInt(strs[2]);
+
+        //dfs();
+
+        //bfs();
+    }
+
+}
